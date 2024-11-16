8a4120cfefd9e786b36aee97e6c420cde885431b
Agathe Makumbi <makagathe7@gmail.com>
2024-11-16 20:05:31 +0100

    save option"

--- font_app/app.js
+++ font_app/app.js
@@ -4,19 +4,18 @@ const input = document.querySelector("input[type='text']");
 const textarea =  document.querySelector(".output");
 const select = document.querySelector("select[name='typeface']");
 const size = document.querySelector("input[name='size']");
+const sizelabel = document.querySelector('.sizelabel');
 const color = document.querySelector(".colors");
 const weight = document.querySelector("input[name='weight']");
+const weightlabel = document.querySelector('.weightLabel');
 const leading = document.querySelector("input[name='leading']");
+const leadinglabel = document.querySelector('.leadinglabel');
 const italic = document.querySelector("input[name='italic']");
 const saveButton = document.querySelector('.save');
-let italicValue = false ;
-let fontFamily;
-let fontSize;
-let fontWeight;
-let lineHeight;
-let texte;
+const settingContainer = document.querySelector('.settings-container');
 let settings = [];
 let setting = {};
+const truncateString = (string = "", maxLength = 50) => string.length > maxLength ? `${string.substring(0, maxLength)}…` : string;
 
 //Interaction entre input et output
 function changeInput(currentInput, currentOutput){
@@ -42,17 +41,20 @@ select.addEventListener('change', function (e) {
 //Changement de taille
 size.addEventListener('change',function (e) {
     textarea.style.fontSize = this.value + 'px';
+    sizelabel.textContent = this.value + 'px';
     setting['size'] = this.value;
 });
 
 // de graisse 
 weight.addEventListener('change',function (e) {
     textarea.style.fontWeight = this.value;
+    weightlabel.textContent = this.value;
     setting['weight'] = this.value;
 } );
 //et d’interlignage
 leading.addEventListener('change',function (e) {
     textarea.style.lineHeight = this.value;
+    leadinglabel.textContent = this.value;
     setting['leading'] = this.value;
 } );
 
@@ -78,6 +80,36 @@ color.addEventListener('click',(e)=>{
 function saveSettings(){
     settings.push(setting);
     console.log(setting);
+    console.log(settings);
+    return setting;
 }
-saveButton.addEventListener('click', saveSettings());
+//Pour chaque réglage sauvegardé, un élément <div> avec la classe “setting” doit apparaitre comme enfant de l’élément avec la classe “settings-container”. 
+const showSetting = (currentSetting) =>{
+    const settingElement = document.createElement("div");
+    settingElement.classList.add('setting');
+    settingElement.style.backgroundColor = currentSetting.bgColor;
+    settingElement.style.color = currentSetting.color;
+    settingElement.style.fontFamily = currentSetting.typeface;
+    settingElement.style.fontStyle = currentSetting.italic ? 'italic' : 'normal';
+    settingElement.style.fontWeight = currentSetting.weight;
+    settingElement.textContent = truncateString(currentSetting.text, 20);
+    settingContainer.appendChild(settingElement);
+    console.log(settingElement);
+    return currentSetting;
+};
+saveButton.addEventListener('click', (e)=>{
+    
+    //creation d'un élément réglage
+    showSetting(saveSettings());
+
+}); 
+
+settingContainer.addEventListener('click', (e)=>{
+    if(e.target.classList.contains('setting')){
+        console.log(e);
+        //.target.nodeType
+    }
+})
+
+
 
